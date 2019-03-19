f4733c3c65003ff82ae3e13ddedccb9a13b04f7c
James Corbett <james.h.corbett@gmail.com>
2019-03-18 21:16:15 -0400

    adding mallocs to sorttest.c

--- sorttest.c
+++ sorttest.c
@@ -51,10 +51,47 @@ long diff_in_ns(struct timespec t1, struct timespec t2) {
   return (diff.tv_sec * 1000000000.0 + diff.tv_nsec);
 }
 
+
+//wrapper function for calling counting_sort with the default arguments
+void quickSortWrapperFunc(int arr[], int arr_length, int arr_range){
+  quick_sort(arr, 0, arr_length - 1);
+}
+
+//wrapper function for calling counting_sort with the default arguments
+void countingSortWrapperFunc(int arr[], int arr_length, int arr_range){
+  counting_sort(arr, arr_length, arr_range);
+} 
+
+//wrapper function for calling insertion_sort with the default arguments
+void insertionSortWrapperFunc(int arr[], int arr_length, int arr_range){
+  insertion_sort(arr, arr_length);
+} 
+
+//wrapper function for calling merge_sort with the default arguments
+void mergeSortWrapperFunc(int arr[], int arr_length, int arr_range){
+  merge_sort(arr, arr_length);
+} 
+
+//wrapper function for calling radix_sort with the default arguments
+void radixSortWrapperFunc(int arr[], int arr_length, int arr_range){
+  radix_sort(arr, arr_length);
+} 
+
+//wrapper function for calling selection_sort with the default arguments
+void selectionSortWrapperFunc(int arr[], int arr_length, int arr_range){
+  selection_sort(arr, arr_length);
+} 
+
+//wrapper function for calling bubble_sort with the default arguments
+void bubbleSortWrapperFunc(int arr[], int arr_length, int arr_range){
+  bubble_sort(arr, arr_length);
+} 
+
+
 int main(int argc, char *argv[]) {
 
-  if(argc != 6) {
-    printf("Usage: ./sorttest <algo_name> <arr_length> <arr_range> <num_reps> <verify?>\n");
+  if(argc != 7) {
+    printf("Usage: ./sorttest <algo_name> <arr_length> <arr_range> <num_reps> <verify?> <initial_mallocs>\n");
     exit(1);
   }
 
@@ -63,42 +100,49 @@ int main(int argc, char *argv[]) {
   int arr_range = atoi(argv[3]);
   int num_reps = atoi(argv[4]);
   char *verify = argv[5];
+  int num_initial_mallocs = atoi(argv[6]);
   int num_correct = 0;
   int arr[arr_length];
-  //create function pointers to cut down on if/else statements in the loop
-  void (*two_arg_sorting_alg)(int[], int) = bubble_sort;  //the two-arg sorting function to call (initialized arbitrarily)
-  void (*three_arg_sorting_alg)(int[], int, int) = quick_sort; //the three-arg sorting function to call (initialized arbitrarily)
-  int arg_two = arr_length; //the second argument to the sorting algorithm
-  int arg_three = -1; //the third argument to the sorting algorithm (if applicable)
 
+  //create function pointer to cut down on if/else statements in the loop
+  void (*sorting_alg)(int[], int, int) = bubbleSortWrapperFunc;  //the two-arg sorting function to call (initialized arbitrarily)
 
   fprintf(stderr, "DEBUG: arr = %p\n", arr);
-
-
   
   //decide on the sorting algorithm
   if (strcmp(algorithm, "bubble") == 0) {
-    two_arg_sorting_alg = bubble_sort;
+    sorting_alg = bubbleSortWrapperFunc;
   } else if (strcmp(algorithm, "counting") == 0) {
-    arg_three = arr_range;
-    three_arg_sorting_alg = counting_sort;
+    sorting_alg = countingSortWrapperFunc;
   } else if (strcmp(algorithm, "insertion") == 0) {
-    two_arg_sorting_alg = insertion_sort;    
+    sorting_alg = insertionSortWrapperFunc;    
   } else if (strcmp(algorithm, "merge") == 0) {
-    two_arg_sorting_alg = merge_sort;
+    sorting_alg = mergeSortWrapperFunc;
   } else if (strcmp(algorithm, "quick") == 0) {
-    arg_two = 0;
-    arg_three = arr_length - 1;
-    three_arg_sorting_alg = quick_sort;
+    sorting_alg = quickSortWrapperFunc;
   } else if (strcmp(algorithm, "radix") == 0) {
-    two_arg_sorting_alg = radix_sort;
+    sorting_alg = radixSortWrapperFunc;
   } else if (strcmp(algorithm, "selection") == 0) {
-    two_arg_sorting_alg = selection_sort;
+    sorting_alg = selectionSortWrapperFunc;
   } else {
     printf("Sorting algorithm not supported.\n");
     exit(1);
   }
-
+ 
+  if(num_initial_mallocs > 0){
+    //allocate num_initial_mallocs' worth of arrays of integers
+    int** ptr = (int**) malloc(num_initial_mallocs * sizeof(int*));
+    //run the initial mallocs
+    for(int i = 0; i < num_initial_mallocs; i++){
+      ptr[i] = (int*) malloc(arr_length * sizeof(int)); 
+    }
+    //now free them
+    for(int i = 0; i < num_initial_mallocs; i++){
+      free(ptr[i]);
+    }
+    //and free the array holding the array pointers
+    free(ptr);
+  }
 
   //run a series of tests for the chosen sorting algorithm
   srand(time(NULL));
@@ -113,11 +157,8 @@ int main(int argc, char *argv[]) {
     struct timespec start, finish;
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
     
-    if (arg_three < 0) {
-      two_arg_sorting_alg(arr, arg_two);
-    } else {
-      three_arg_sorting_alg(arr, arg_two, arg_three);
-    }
+    //run whichever sorting algorithm we picked
+    sorting_alg(arr, arr_length, arr_range);
 
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &finish);
 
